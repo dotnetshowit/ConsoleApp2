1e1a46e938ddd15bc0813a02e39d956375ce9674
dotnetshowit <dotnetshowit@gmail.com>
2024-01-18 23:26:11 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary2;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
